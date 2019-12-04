977679e8deef3680f6d1f70a929663904e7f9f37
sserichenko <sserichenko@gmail.com>
2019-12-04 13:53:26 +0200

    Updates

---
+++ precache-manifest.ab55e83996cbee0cd3b994d788f2d0e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c8dd7f58181825734061947f278de9c",
+    "url": "/goit-react-hw-04-reader/index.html"
+  },
+  {
+    "revision": "d0076614ae24717730ba",
+    "url": "/goit-react-hw-04-reader/static/css/reader-page.8337dd3e.chunk.css"
+  },
+  {
+    "revision": "5e2ecbae26e68ebd4cf4",
+    "url": "/goit-react-hw-04-reader/static/js/3.a21e40e8.chunk.js"
+  },
+  {
+    "revision": "563584d7c295e7452e4a",
+    "url": "/goit-react-hw-04-reader/static/js/main.2291686e.chunk.js"
+  },
+  {
+    "revision": "d0076614ae24717730ba",
+    "url": "/goit-react-hw-04-reader/static/js/reader-page.4333cfac.chunk.js"
+  },
+  {
+    "revision": "a64f3beee5714e85dbe8",
+    "url": "/goit-react-hw-04-reader/static/js/runtime~main.66bc23a0.js"
+  }
+]);
